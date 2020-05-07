65f0bb7b6d401d6d6e33d980e27a2cc9f4357fba
gosha <gosha@ITSWL017.its.az>
2020-05-07 15:36:43 +0400

    replace qoqal to qosha

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
-docker build -t qoqal/multi-client:latest -t qoqal/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t qoqal/multi-server:latest -t qoqal/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t qoqal/multi-worker:latest -t qoqal/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t qosha/multi-client:latest -t qosha/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t qosha/multi-server:latest -t qosha/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t qosha/multi-worker:latest -t qosha/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push qoqal/multi-client:latest
-docker push qoqal/multi-server:latest
-docker push qoqal/multi-worker:latest
+docker push qosha/multi-client:latest
+docker push qosha/multi-server:latest
+docker push qosha/multi-worker:latest
 
-docker push qoqal/multi-client:$SHA
-docker push qoqal/multi-server:$SHA
-docker push qoqal/multi-worker:$SHA
+docker push qosha/multi-client:$SHA
+docker push qosha/multi-server:$SHA
+docker push qosha/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/client-deployment client=qoqal/multi-client:$SHA
-kubectl set image deployments/server-deployment server=qoqal/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=qoqal/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=qosha/multi-client:$SHA
+kubectl set image deployments/server-deployment server=qosha/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=qosha/multi-worker:$SHA
